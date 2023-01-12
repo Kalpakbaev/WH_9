1aeba49e591353a9a1444f6793553c662d303734
Kalpakbaev <kozha_1992@mail.ru>
2023-01-12 07:10:26 +0500

    Ex01

---
+++ Example_01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+        //  Задача 64: Написать рекурсивную функцию перевода из десятичной системы исчисления в
+        //   двоичную. 
+       
+      void decnumbers(int n){
+        if(n > 0){
+          if(n%2 == 1) decnumbers(n/2);
+        }
+            Console.Write(n +"");
+      }
+     
+      Console.Write("Enter your naumber: ");
+      int n = Convert.ToInt32(Console.ReadLine());
+     
+ 
+        
